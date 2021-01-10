fcec07dce5af29a1ba641b8ce5cd0e57f78cc103
Adam Jonas <adamjonas25@gmail.com>
2021-01-10 15:16:44 +0000

    remove comments

--- zombie_spec.rb
+++ zombie_spec.rb
@@ -9,17 +9,14 @@ describe Zombie do
   describe Zombie do
 
     it 'is named Ash' do
-      # zombie = Zombie.new - replaced this with the before :each block above. used instance var so it's accessible to all specs
       expect(@zombie.name).to eq('Ash')
     end
 
     it 'has no brains' do
-      # zombie = Zombie.new
       expect(@zombie.brains).to be < 1
     end
 
     it 'is not alive' do
-      # zombie = Zombie.new
       expect(@zombie).not_to be_alive
     end
 
